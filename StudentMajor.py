b201c3c19088c4bae596b16334a9c695b8774fa8
angedesu <104692309+angedesu@users.noreply.github.com>
2023-11-29 14:46:23 -0800

    Create StudentMajor.py

---
+++ StudentMajor.py
@@ -0,0 +1,10 @@
+# collection 
+ studentMajors = db['studentMajors']
+    studentMajor_count = studentMajors.count_documents({})
+    print(f'Student Majors in collection so far: {studentMajor_count}')
+
+
+# unique index 
+
+
+# schema 
