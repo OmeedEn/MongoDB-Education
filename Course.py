190ceccd947afcc5398a85936dd964f06eeadffa
angedesu <104692309+angedesu@users.noreply.github.com>
2023-11-29 14:40:24 -0800

    Update Course.py

---
+++ Course.py
@@ -0,0 +1,12 @@
+# collections
+courses = db['courses']
+    course_count = courses.count_documents({})
+    print(f'Courses in the collection so far: {course_count}')
+
+
+# unique index 
+
+
+# schema 
+
+
