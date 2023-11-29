1b98014fdabbc9bbbfe02cb0a89062ae4926722b
justin-o-beup <110871497+justin-o-beup@users.noreply.github.com>
2023-11-29 14:56:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import pymongo
-import
+import logging
+from constants import *
+from menu_definitions import menu_main, add_menu, delete_menu, list_menu, debug_select, introspection_select
+from IntrospectionFactory import IntrospectionFactory
+from db_connection import engine, Session
+from orm_base import metadata
+# Note that until you import your SQLAlchemy declarative classes, such as Student, Python
+# will not execute that code, and SQLAlchemy will be unaware of the mapped table.
+from Department import Department
+from Course import Course
+from Major import Major
+from Student import Student
+from StudentMajor import StudentMajor
+from Option import Option
+from Menu import Menu
+from Section import Section
+from Enrollment import Enrollment
+from datetime import time
 
