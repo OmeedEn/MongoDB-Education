9d09b8d666dc6565f6278c6773182d598e5db487
angedesu <104692309+angedesu@users.noreply.github.com>
2023-11-29 14:38:38 -0800

    Update Student.py

---
+++ Student.py
@@ -0,0 +1,29 @@
+# collections 
+students = db["students"]
+    student_count = students.count_documents({})
+    print(f"Students in the collection so far: {student_count}")
+
+
+# uniqe index 
+
+students_indexes = students.index_information()
+    if 'students_last_and_first_names' in students_indexes.keys():
+        print("first and last name index present.")
+    else:
+        # Create a single UNIQUE index on BOTH the last name and the first name.
+        students.create_index([('last_name', pymongo.ASCENDING), ('first_name', pymongo.ASCENDING)],
+                              unique=True,
+                              name="students_last_and_first_names")
+    if 'students_e_mail' in students_indexes.keys():
+        print("e-mail address index present.")
+    else:
+        # Create a UNIQUE index on just the e-mail address
+        students.create_index([('e_mail', pymongo.ASCENDING)],
+                              unique=True,
+                              name='students_e_mail')
+    pprint(students.index_information())
+
+
+# schema
+
+
