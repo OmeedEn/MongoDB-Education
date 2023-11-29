aef5c230f7f1f011305f4b44138f9d527917d05d
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-11-28 22:12:45 -0800

    uploads
    uploading what we already did in mongoDB single collection project

---
+++ menu_definitions.py
@@ -0,0 +1,47 @@
+
+from Menu import Menu
+from Option import Option
+"""
+This little file just has the menus declared. Each variable (e.g. menu_main) has
+its own set of options and actions. Although, you'll see that the "action" could
+be something other than an operation to perform.
+Doing the menu declarations here seemed like a cleaner way to define them. When
+this is imported in main.py, these assignment statements are executed and the
+variables are constructed. To be honest, I'm not sure whether these are global
+variables or not in Python.
+"""
+# The main options for operating on Departments and Courses.
+menu_main = Menu('main', 'Please select one of the following options:', [
+    Option("Add", "add(db)"),
+    Option("List", "list_objects(db)"),
+    Option("Delete", "delete(db)"),
+    # Option("Boilerplate Data", "boilerplate(db)"),
+    Option("Exit this application", "pass")
+])
+add_menu = Menu('add', 'Please indicate what you want to add:', [
+    Option("Department", "add_department(db)"),
+    # Option("Course", "add_course(db)"),
+    # Option("Major", "add_major(db)"),
+    Option("Student", "add_student(db)"),
+    # Option("Student to Major", "add_student_major(db)"),
+    # Option("Major to Student", "add_major_student(db)"),
+    Option("Exit", "pass")
+])
+delete_menu = Menu('delete', 'Please indicate what you want to delete from:', [
+    Option("Department", "delete_department(db)"),
+    # Option("Course", "delete_course(db)"),
+    # Option("Major", "delete_major(db)"),
+    Option("Student", "delete_student(db)"),
+    # Option("Student to Major", "delete_student_major(db)"),
+    # Option("Major to Student", "delete_major_student(db)"),
+    Option("Exit", "pass")
+])
+list_menu = Menu('list', 'Please indicate what you want to list:', [
+    Option("Department", "list_department(db)"),
+    # Option("Course", "list_course(db)"),
+    # Option("Major", "list_major(db)"),
+    Option("Student", "list_student(db)"),
+    # Option("Student to Major", "list_student_major(db)"),
+    # Option("Major to Student", "list_major_student(db)"),
+    Option("Exit", "pass")
+])
