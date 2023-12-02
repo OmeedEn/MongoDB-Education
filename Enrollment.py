99c63890bffc227bc979cdd9ab6e3d8fc7078965
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-02 14:19:43 -0800

    Delete Enrollment.py

--- Enrollment.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-from orm_base import Base
-from sqlalchemy import String, Integer, UniqueConstraint
-from sqlalchemy import ForeignKey, ForeignKeyConstraint
-from sqlalchemy.orm import Mapped, mapped_column, relationship
