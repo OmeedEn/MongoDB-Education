2e8360f9d2da2227c191aab1f64ebbcb8ccb5db8
angedesu <104692309+angedesu@users.noreply.github.com>
2023-12-02 11:52:45 -0800

    Update Major.py

--- Major.py
+++ Major.py
@@ -4,7 +4,16 @@ majors = db['majors']
     print(f'Majors in collection so far: {major_count}')
 
 # unique index 
-
+majors_indexes = majors.index_information()
+    
+    if 'name' in majors_indexes.keys():
+        print('major name index present')
+        
+    else:
+        # create a single UNIQUE index on major name
+        majors.create_index([('name', pymongo.ASCENDING)],
+                            unique=True,
+                            name='name')
 
 # schema 
 
