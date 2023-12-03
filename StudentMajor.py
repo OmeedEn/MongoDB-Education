2bb82c3ae3847ba209e7ac3f6a03b75e5ed41fca
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-02 18:51:09 -0800

    added the schema to the studentmajor file

--- StudentMajor.py
+++ StudentMajor.py
@@ -8,3 +8,20 @@
 
 
 # schema 
+student_major_validator = {
+    'validator': {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'description': 'A major dedicated to a student',
+            'required': ['declarationDate'],
+            'additionalProperties': False,
+            'properties': {
+                '_id': {},
+                'declarationDate': {
+                    'bsonType': 'date',
+                    'description': 'The date the Student declared his/her major. '
+                }
+            }
+        }
+    }
+}
