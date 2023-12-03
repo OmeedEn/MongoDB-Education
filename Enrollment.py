8a900cb88ffc0439bb8cecb989ced3783a11c8f2
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-02 16:11:44 -0800

    added the correct enrollment file

---
+++ Enrollment.py
@@ -0,0 +1,11 @@
+# collections
+enrollments = db['enrollments']
+    enrollment_count = enrollments.count_documents({})
+    print(f'Enrollments in collection so far: {enrollment_count}')
+
+# unique index 
+
+
+# schema 
+#im not sure if we need schemas or relations to the other passfail/lettergrade files
+
