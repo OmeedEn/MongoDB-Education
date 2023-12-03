2cfed73b78da9ccde76729ce3e0ff6bdb3fe9a45
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-02 18:46:07 -0800

    added the schema for the course.py file

--- Course.py
+++ Course.py
@@ -8,5 +8,41 @@ courses = db['courses']
 
 
 # schema 
+course_validator = {
+    'validator': {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'description': 'A course in a department.',
+            'required': ['courseNumber', 'courseName','description', 'units'],
+            'additionalProperties': False,
+            'properties': {
+                '_id': {},
+                'courseNumber': {
+                    'bsonType': 'number',
+                    'minLength': 1,
+                    'maxLength': 6,
+                    'description': 'The course ID that is unique to that course.'
+                },
+                'courseName': {
+                    'bsonType': 'string',
+                    'minLength': 1,
+                    'maxLength': 50,
+                    'description': 'The word that determines the course.'
+                },
+                'description': {
+                    'bsonType': 'string',
+                    'minLength': 10,
+                    'maxLength': 80,
+                    'description': 'Statement that describes the course.'
+                },
+                'units': {
+                    'bsonType': 'number',
+                    'description': 'Amount of units a specific course has.'
+                },
+            }
+
+        }
+    }
+}
 
 
