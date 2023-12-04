e0ff3ea8fbcfba2339ff70efc89f5905bf394722
angedesu <104692309+angedesu@users.noreply.github.com>
2023-12-04 15:05:01 -0800

    Update Enrollment.py
    added the uniqueness

--- Enrollment.py
+++ Enrollment.py
@@ -4,6 +4,25 @@ enrollments = db['enrollments']
     print(f'Enrollments in collection so far: {enrollment_count}')
 
 # unique index 
+enrollments_index = enrollments.index_information()
+
+    if 'student_and_section' in enrollments_index.keys():
+        print('student and section index present')
+    else:
+        # create a UNIQUE index on student and section
+        enrollments.create_index([('student', pymongo.ASCENDING), ('section', pymongo.ASCENDING)],
+                                 unique=True,
+                                 name='student_and_sections')
+    if 'semester_sectionYear_departmentAbbreviation_courseNumber_studentID' in enrollments_index.keys():
+        print('semester, section year, department abbreviation, course number, student ID index present')
+    else:
+        # create a UNIQUE index on semester, section year, department abbreviation, course number, student ID
+        enrollments.create_index([('semester', pymongo.ASCENDING), ('sectionYear', pymongo.ASCENDING),
+                                  ('departmentAbbreviation', pymongo.ASCENDING), ('courseNumber', pymongo.ASCENDING),
+                                  ('studentID', pymongo.ASCENDING)],
+                                 unique=True,
+                                 name='semester_sectionYear_departmentAbbreviation_courseNumber_studentIDs')
+    pprint(enrollments.index_information())
 
 
 # schema 
