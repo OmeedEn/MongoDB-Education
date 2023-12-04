8bf8f45455fa2e76cc2bbd023e51b43f8b2f5549
angedesu <104692309+angedesu@users.noreply.github.com>
2023-12-04 14:04:34 -0800

    Update Section.py

--- Section.py
+++ Section.py
@@ -1,5 +1,7 @@
 # creating collection
-
+sections = db['sections']
+    section_count = sections.count_documents({})
+    print(f'Sections in collection so far: {section_count}')
 
 # unique indexes 
 
