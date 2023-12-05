7c57d24b3bf2073a2cefc2bea7691ebc85951262
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-05 15:50:20 -0800

    created the schema for the letterGrade

--- LetterGrade.py
+++ LetterGrade.py
@@ -1,1 +1,20 @@
+import pymongo
+import pprint
 
+letter_grade_validator = {
+    'validator': {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'description': 'A letter grade given to a specific course. ',
+            'required': ['_id', 'minSatisfactory'],
+            'additionalProperties': False,
+            'properties': {
+                '_id': {},
+                'minSatisfactory': {
+                    'bsonType': 'string',
+                    'description': 'the minimum grade that would be satisfactory'
+                }
+            }
+        }
+    }
+}
