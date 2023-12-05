1c55e617fe9a230767591a80536673bb98237bce
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-05 15:54:52 -0800

    created the schema for the pass fail class

--- PassFail.py
+++ PassFail.py
@@ -1,1 +1,21 @@
+import pymongo
+import pprint
+
+passFail_validator = {
+    'validator': {
+        '$jsonSchema': {
+            'bsonType': 'object',
+            'description': 'Determines whether the student is pass/fail for a specific enrollment.',
+            'required': ['_id', 'applicationDate'],
+            'additionalProperties': False,
+            'properties': {
+                '_id': {},
+                'applicationDate': {
+                    'bsonType': 'date',
+                    'description': 'the date the application was put in'
+                }
+            }
+        }
+    }
+}
 
