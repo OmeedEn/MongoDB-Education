15342fcecc070f4e184f231b66a34d9fc7583e32
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-07 14:36:19 -0800

    changed the format of the schemas

--- Enrollment.py
+++ Enrollment.py
@@ -31,52 +31,102 @@ pprint(enrollments.index_information())
 # schema 
 #im not sure if we need schemas or relations to the other passfail/lettergrade files
 
-enrollment_validator = {
-    'validator': {
-        '$jsonSchema': {
+
+enrollment_validator2 = {
+    'oneOf': [
+        {
             'bsonType': 'object',
-            'description': 'An enrollment for each student',
-            'required': ['enrollment'],
+            'required': ['application_date'],
             'additionalProperties': False,
             'properties': {
-                '_id': {},
-                'enrollment': {
-                    'bsonType': 'object',
-                    'description': 'The enrollment for the students',
-                    'properties': {
-                        'passFail': {
-                            'bsonType': 'array',
-                            'items': {
-                                'bsonType': 'object',
-                                'description': 'Determines whether a student can pass or fail',
-                                'required': ['applicationDate'],  # Add other required fields if any
-                                'properties': {
-                                    'applicationDate': {
-                                        'bsonType': 'date',
-                                        'description': 'The date the student filed for pass or fail'
-                                    },
-                                }
-                            }
-                        },
-                        'letterGrade': {
-                            'bsonType': 'array',
-                            'items': {
-                                'bsonType': 'object',
-                                'description': 'The letter grade for each student that is enrolling',
-                                'required': ['minSatisfactory'],
-                                'properties': {
-                                    'minSatisfactory': {
-                                        'bsonType': 'string',
-                                        'enum': ['A', 'B', 'C'],
-                                        'description': 'The minimum grade that would be satisfactory'
-                                    }
-                                }
-                            }
-                        }
-                    }
+                'application_date': {
+                    'bsonType': 'date',
+                    'description': 'The date the student filed for pass or fail'
+                }
+            }
+        },
+        {
+            'bsonType': 'object',
+            'required': ['min_satisfactory'],
+            'additionalProperties': False,
+            'properties': {
+                'min_satisfactory': {
+                    'bsonType': 'string',
+                    'enum': ['A', 'B', 'C'],
+                    'description': 'The minimum grade that would be satisfactory'
                 }
             }
         }
+    ]
+}
+student_validator = {
+    'bsonType': 'object',
+    'required': ['student_id'],
+    'additionalProperties': False,
+    'properties': {
+        'student_id': {
+            'bsonType': 'objectId', #not too sure if its different from the object
+            'description': 'unique identifier of students'
+        }
+    }
+}
+enrollment_valid = {
+    'bsonType': "object",
+    'description': 'the enrollment of the students',
+    'required': ['students', 'enrollment_data'],
+    'additionalProperties': False,
+    'properties': {
+        'students': student_validator,
+        'enrollment': enrollment_validator2
     }
 }
 
+# enrollment_validator = {
+#     'validator': {
+#         '$jsonSchema': {
+#             'bsonType': 'object',
+#             'description': 'An enrollment for each student',
+#             'required': ['enrollment'],
+#             'additionalProperties': False,
+#             'properties': {
+#                 '_id': {},
+#                 'enrollment': {
+#                     'bsonType': 'object',
+#                     'description': 'The enrollment for the students',
+#                     'additionalProperties': False,
+#                     'properties': {
+#                         'passFail': {
+#                             'bsonType': 'array',
+#                             'items': {
+#                                 'bsonType': 'object',
+#                                 'description': 'Determines whether a student can pass or fail',
+#                                 'required': ['applicationDate'],  # Add other required fields if any
+#                                 'properties': {
+#                                     'applicationDate': {
+#                                         'bsonType': 'date',
+#                                         'description': 'The date the student filed for pass or fail'
+#                                     },
+#                                 }
+#                             }
+#                         },
+#                         'letterGrade': {
+#                             'bsonType': 'array',
+#                             'items': {
+#                                 'bsonType': 'object',
+#                                 'description': 'The letter grade for each student that is enrolling',
+#                                 'required': ['minSatisfactory'],
+#                                 'properties': {
+#                                     'minSatisfactory': {
+#                                         'bsonType': 'string',
+#                                         'enum': ['A', 'B', 'C'],
+#                                         'description': 'The minimum grade that would be satisfactory'
+#                                     }
+#                                 }
+#                             }
+#                         }
+#                     }
+#                 }
+#             }
+#         }
+#     }
+# }
