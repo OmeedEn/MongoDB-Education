cb706bc9d7a310adb67f4f546c0b3d7a429deb5b
Omeed Enshaie <107780070+OmeedEn@users.noreply.github.com>
2023-12-07 00:19:33 -0800

    added imports

--- StudentMajor.py
+++ StudentMajor.py
@@ -1,19 +1,23 @@
-# collection 
- studentMajors = db['studentMajors']
-    studentMajor_count = studentMajors.count_documents({})
-    print(f'Student Majors in collection so far: {studentMajor_count}')
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+
+# collection
+studentMajors = db['studentMajors']
+studentMajor_count = studentMajors.count_documents({})
+print(f'Student Majors in collection so far: {studentMajor_count}')
 
 
 # unique index 
 studentMajors_index = studentMajors.index_information()
-    if 'student_and_majorName' in studentMajors_index.keys():
-        print('student and major name index present')
-    else:
-        # create a single UNIQUE index on student and majorName
-        studentMajors.create_index([('student', pymongo.ASCENDING), ('majorName', pymongo.ASCENDING)],
-                                   unique=True,
-                                   name='student_and_majorNames')
-    pprint(studentMajors.index_information())
+if 'student_and_majorName' in studentMajors_index.keys():
+    print('student and major name index present')
+else:
+    # create a single UNIQUE index on student and majorName
+    studentMajors.create_index([('student', pymongo.ASCENDING), ('majorName', pymongo.ASCENDING)],
+                               unique=True,
+                               name='student_and_majorNames')
+pprint(studentMajors.index_information())
 
 
 # schema 
