71ffd085df79a48a1c6f06816510b79b27509ff3
angedesu <104692309+angedesu@users.noreply.github.com>
2023-12-10 09:22:02 -0800

    Update Enrollment.py
    fixed the schema to match the indexes. also updated the add functions

--- Enrollment.py
+++ Enrollment.py
@@ -163,3 +163,192 @@ def create_enrollment(db):
         }
     }
     db.command('collMod', 'enrollments', validator=enrollment_validator)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+from datetime import datetime
+from pymongo.errors import CollectionInvalid, OperationFailure
+import Student
+import Section
+
+
+#import function
+def add_letter_grade(db):
+    collection = db["enrollments"]
+    unique_enrollment = False
+    while not unique_enrollment:
+        student = Student.select_student(db)
+        section = Section.select_section(db)
+        grade = input("What is the Satisfactory grade?---> ")
+
+        enrollment_count = collection.count_documents({"studentId": student['_id'], "sectionId": section['_id']})
+        unique_enrollment = enrollment_count == 0
+        if not unique_enrollment:
+            print("No enrollment record found for that student and section. Try again.")
+        if unique_enrollment:
+            enrollment_type = {'minSatisfactory': grade}
+            enrollment = {
+                'studentId': student['_id'],
+                'sectionId': section['_id'],
+                'departmentAbbreviation': section['departmentAbbreviation'],
+                'courseNumber': section['courseNumber'],
+                'sectionYear': section['sectionYear'],
+                'semester': section['semester'],
+                'enrollmentType': type}
+            collection.insert_one(enrollment)
+
+def add_student_pass_fail(db):
+    collection = db["enrollments"]
+    unique_enrollment = False
+
+    while not unique_enrollment:
+        student = Student.select_student(db)
+        section = Section.select_section(db)
+        enrollment_count = collection.count_documents({"studentId": student['_id'], "sectionId": section['_id']})
+        unique_enrollment = enrollment_count == 0
+        if not unique_enrollment:
+            print('No enrollment record found for that student and section. Try again.')
+        if unique_enrollment:
+            enrollment = {
+                'studentId': student['_id'],
+                'sectionId': section['_id'],
+                'application_date': datetime.now()
+            }
+            collection.insert_one(enrollment)
+
+def select_enrollment(db):
+
+    collection = db["enrollments"]
+    found: bool = False
+    student = Student.select_student(db)
+    section = Section.select_section(db)
+
+    while not found:
+        found_count: int = collection.count_documents({"studentId": student['_id'],
+                                                       "sectionId": section["_id"]})
+        found = found_count == 1
+    if not found:
+        print("No Student found with that section. Try again. ")
+    found_enrollment = collection.find_one({"studentId": student['_id'],
+                                            "sectionId": section["_id"]})
+    return found_enrollment
+def delete_enrollment(db):
+
+    enrollment = select_enrollment(db)
+    enrollments = db["enrollments"]
+
+    deleted = enrollments.delete_one({"_id": enrollment["_id"]})
+
+    print(f"We just deleted: {deleted.deleted_count} enrollments.")
+def list_enrollment(db):
+    enrollments = db["enrollments"].find({}).sort([("_id", pymongo.ASCENDING)])
+
+    for enrollment in enrollments:
+        pprint(enrollment)
+# schema function
+def create_enrollment(db):
+    # collections
+    enrollments = db['enrollments']
+    enrollment_count = enrollments.count_documents({})
+    print(f'Enrollments in collection so far: {enrollment_count}')
+
+    # unique index
+    enrollments_index = enrollments.index_information()
+
+    if 'studentId_and_sectionId' in enrollments_index.keys():
+        print('student and section ID\'s index present')
+    else:
+        # create a UNIQUE index on student and section
+        enrollments.create_index([('studentId', pymongo.ASCENDING), ('sectionId', pymongo.ASCENDING)],
+                                 unique=True,
+                                 name='student_and_sections')
+    if 'semester_sectionYear_departmentAbbreviation_courseNumber_studentID' in enrollments_index.keys():
+        print('semester, section year, department abbreviation, course number, student ID index present')
+    else:
+        # create a UNIQUE index on semester, section year, department abbreviation, course number, student ID
+        enrollments.create_index([('semester', pymongo.ASCENDING), ('sectionYear', pymongo.ASCENDING),
+                                  ('departmentAbbreviation', pymongo.ASCENDING), ('courseNumber', pymongo.ASCENDING),
+                                  ('studentID', pymongo.ASCENDING)],
+                                 unique=True,
+                                 name='semester_sectionYear_departmentAbbreviation_courseNumber_studentIDs')
+    pprint(enrollments.index_information())
+    # schema
+    enrollment_validator = {
+        'bsonType': "object",
+        'description': 'the enrollment of the students',
+        'required': ['studentId', 'sectionId', 'departmentAbbreviation', 'courseNumber', 'sectionYear', 'semester',
+                     'enrollmentType'],
+        'additionalProperties': False,
+        'properties': {
+            'studentId': {
+                'bsonType': 'objectId',
+                'description': 'The students\' Id.'
+            },
+            'sectionId': {
+                'bsonType': 'objectId',
+                'description': 'The sections\' Id.'
+            },
+            'departmentAbbreviation': {
+                'bsonType': 'string',
+                'description': 'The department of the section the student is enrolling in.'
+            },
+            'courseNumber': {
+                'bsonType': 'number',
+                'description': 'The courseNumber of the section that student is enrolling in.'
+            },
+            'sectionYear': {
+                'bsonType': 'number',
+                'description': 'The section year of the section the student is enrolling in.'
+            },
+            'semester': {
+                'bsonType': 'string',
+                'description': 'The semester of the section the student is enrolling in.'
+            },
+            'enrollmentType': {
+                'oneOf': [
+                    {
+                        'bsonType': 'object',
+                        'required': ['application_date'],
+                        'additionalProperties': True,
+                        'properties': {
+                            'applicationDate': {
+                                'bsonType': 'date',
+                                'description': 'The date the student filed for pass or fail'
+                            }
+                        }
+                    },
+                    {
+                        'bsonType': 'object',
+                        'required': ['min_satisfactory'],
+                        'additionalProperties': True,
+                        'properties': {
+                            'minSatisfactory': {
+                                'bsonType': 'string',
+                                'enum': ['A', 'B', 'C'],
+                                'description': 'The minimum grade that would be satisfactory'
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    db.command('collMod', 'enrollments', validator=enrollment_validator)
+
+
